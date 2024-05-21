4fbe1c3dcdcf6589a7598e4365f2eb1f78291ce2
Someswar Gorai <somgorai726@gmail.com>
2024-05-21 18:46:22 +0530

    updated

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(
     })
   ); 
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+  app.use(express.json({ limit: '50mb' })); 
+  app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.get('/',(req,res)=>{
     res.send(`<h1>Hello World!</h1>`);
